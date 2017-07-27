3932e568edea4a8e49dce3767b3c7fdcbcd4c4d6
Giovanni <giofusco@ski.org>
2017-07-27 02:46:16 -0700

    using core function in elon libraries. Need to implement check on indexes in elon library. QUESTION: Why -11 in range?

--- elon_filters.py
+++ elon_filters.py
@@ -1,10 +1,14 @@
 import numpy as np
+import pyglitch.core as pgc
 
 def epoplectic(i, xshift, yshift):
 
-    ydim = i.shape[0]
-    xdim = i.shape[1]
+    ydim = pgc.height(i)
+    xdim = pgc.width(i)
 
+    # TODO: check range of indexes when accessing matrix i
+    #       why -11 in range?
+    #
     for x in range(xdim-11):
         for y in range(ydim-11):
             if np.sum(i[y,x]) > np.sum(i[y+yshift,x+xshift]):
