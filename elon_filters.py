feda5e1a4274dd46c76ae6577be77a211bc56b3c
Giovanni <giofusco@ski.org>
2017-07-29 23:37:03 -0700

    Fixed some formatting and removed old comments from elon_filters.py

--- elon_filters.py
+++ elon_filters.py
@@ -1,20 +1,16 @@
 import numpy as np
 import pyglitch.core as pgc
 
+
 def epoplectic(i, xshift, yshift):
+    ydim = pgc.height(i)
+    xdim = pgc.width(i)
 
-	
+    assert xdim - xshift > 0 and xshift > 0, "xshift must be between 0-%i" %xdim
+    assert ydim - yshift > 0 and yshift > 0, "yshift must be between 0-%i" %ydim
 
-	ydim = pgc.height(i)
-	xdim = pgc.width(i)
-
-	assert xdim-xshift > 0 and xshift > 0, "xshift must be between 0-%i" %xdim
-	assert ydim-yshift > 0 and yshift > 0, "yshift must be between 0-%i" %ydim
-    # TODO: check range of indexes when accessing matrix i
-    #       why -11 in range?
-    #
-	for x in range(xdim-xshift):
-		for y in range(ydim-yshift):
-			if np.sum(i[y,x]) > np.sum(i[y+yshift,x+xshift]):
-				i[y,x] = i[y+yshift,x+xshift]
-	return i
+    for x in range(xdim - xshift):
+        for y in range(ydim - yshift):
+            if np.sum(i[y,x]) > np.sum(i[y + yshift, x+xshift]):
+                i[y, x] = i[y + yshift, x+xshift]
+    return i
