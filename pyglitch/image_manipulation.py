fixed image normalization issue

pyglitch/image_manipulation.py
@@ -29,7 +29,8 @@ DIRECTION_DOWN = 107
 
 
 # TODO: handle different kinds of paddings
-def shift_rows(I, start, num_rows, offset, padding, color=(0,0,0)):
+def shift_rows(X, start, num_rows, offset, padding, color=(0,0,0)):
+    I = X.copy()
     pad = None
     if padding == PADDING_CIRCULAR:
         rows = np.roll(I[start:start+num_rows-1,0:-1], offset, axis=1)
@@ -37,7 +38,8 @@ def shift_rows(I, start, num_rows, offset, padding, color=(0,0,0)):
     return I
 
 
-def swap_channels_at(I, x, y, w, h, channel_idxs):
+def swap_channels_at(X, x, y, w, h, channel_idxs):
+    I = X.copy()
     patch = pgc.get_patch(I, x, y, w, h)
     if patch is not None:
         swap_channels(patch[4], channel_idxs)
@@ -46,7 +48,8 @@ def swap_channels_at(I, x, y, w, h, channel_idxs):
     return I
 
 
-def swap_channels(I, channel_idxs):
+def swap_channels(X, channel_idxs):
+    I = X.copy()
     c0 = I[:-1, :-1, channel_idxs[0]].copy()
     c1 = I[:-1, :-1, channel_idxs[1]].copy()
     c2 = I[:-1, :-1, channel_idxs[2]].copy()
@@ -56,7 +59,8 @@ def swap_channels(I, channel_idxs):
     return I
 
 
-def shift_channel_hor_at(I, x, y, w, h, channel_idx, offset):
+def shift_channel_hor_at(X, x, y, w, h, channel_idx, offset):
+    I = X.copy()
     patch = pgc.get_patch(I, x, y, w, h)
     if patch is not None:
         shift_channel_hor(patch[4], channel_idx, offset)
@@ -64,19 +68,22 @@ def shift_channel_hor_at(I, x, y, w, h, channel_idx, offset):
     return I
 
 
-def shift_channel_hor(I, channel_idx, offset):
+def shift_channel_hor(X, channel_idx, offset):
+    I = X.copy()
     rows = np.roll(I[:-1,:-1,channel_idx], offset, axis=1)
     I[:-1, :-1, channel_idx] = rows
     return I
 
 
-def shift_channel_ver(I, channel_idx, offset):
+def shift_channel_ver(X, channel_idx, offset):
+    I = X.copy()
     cols = np.roll(I[:-1,:-1,channel_idx], offset, axis=0)
     I[:-1, :-1, channel_idx] = cols
     return I
 
 
-def shift_channel_ver_at(I, x, y, w, h, channel_idx, offset):
+def shift_channel_ver_at(X, x, y, w, h, channel_idx, offset):
+    I = X.copy()
     patch = pgc.get_patch(I, x, y, w, h)
     if patch is not None:
         shift_channel_ver(patch[4], channel_idx, offset)
@@ -84,28 +91,32 @@ def shift_channel_ver_at(I, x, y, w, h, channel_idx, offset):
     return I
 
 
-def saturate_channel_at(I, x, y, w, h, channel_idx):
+def saturate_channel_at(X, x, y, w, h, channel_idx):
+    I = X.copy()
     """saturate a channel in a specific image patch"""
     patch = pgc.get_patch(I, x, y, w, h)
     if patch is not None:
-        saturate_channel(patch[4], channel_idx)
+        I = saturate_channel(patch[4], channel_idx)
         pgc.put_patch_in_place(I, patch)
     return I
 
 
-def saturate_channel(I, channel_idx):
+def saturate_channel(X, channel_idx):
+    I = X.copy()
     """"saturates a channel of the image"""
-    set_channel_value(I, channel_idx, 255)
+    I = set_channel_value(I, channel_idx, 255)
     return I
 
 
-def set_channel_value(I, channel_idx, value):
+def set_channel_value(X, channel_idx, value):
+    I = X.copy()
     if channel_idx < pgc.num_channels(I):
         I[:-1,:-1,channel_idx] = value
     return I
 
 
-def posterize(I, num_bins, normalize=True):
+def posterize(X, num_bins, normalize=True):
+    I = X.copy()
     if normalize:
         I = rescale_image(I)
     bin_size = int(255/num_bins)
@@ -113,17 +124,18 @@ def posterize(I, num_bins, normalize=True):
     return I.astype(np.uint8)
 
 
-def pixel_sort_brighter_than_rgb_vert(I, r, g, b, strict=False, sorting_order=('h', 'l', 'v'), iterations=8):
-    I = pixel_sort_brighter_than_rgb(pgc.rotate_right(I), r, g, b,
+def pixel_sort_brighter_than_rgb_vert(X, r, g, b, strict=False, sorting_order=('h', 'l', 'v'), iterations=8):
+    I = pixel_sort_brighter_than_rgb(pgc.rotate_right(X), r, g, b,
                                      strict=False, sorting_order=('h', 'l', 'v'), iterations=8)
     I = pgc.rotate_left(I)
     return I
 
 
 @jit
-def pixel_sort_brighter_than_rgb(I, r, g, b, strict=False, sorting_order=('h', 'l', 'v'), iterations=8):
+def pixel_sort_brighter_than_rgb(X, r, g, b, strict=False, sorting_order=('h', 'l', 'v'), iterations=8):
     """begin sorting when it finds a pixel which is not (r,g,b) in the column or row,
         and will stop sorting when it finds a (r,g,b) pixel"""
+    I = X.copy()
     for row in prange(0, pgc.height(I)):
         if strict:
             from_idx = np.argwhere((I[row, :-1, pgc.CH_RED] > r) & (I[row, :-1, pgc.CH_GREEN] > g)
@@ -153,7 +165,8 @@ def pixel_sort_brighter_than_rgb(I, r, g, b, strict=False, sorting_order=('h', '
 
 
 # not sure it's necessary
-def rescale_image_rgb(I):
+def rescale_image_rgb(X):
+    I = X.copy()
     I_r = rescale_image(I[:,:,0])
     I_g = rescale_image(I[:, :, 1])
     I_b = rescale_image(I[:, :, 2])
@@ -161,7 +174,8 @@ def rescale_image_rgb(I):
     return  I.astype(np.uint8)
 
 
-def rescale_image(I):
+def rescale_image(X):
+    I = X.copy()
     if (I.min() < 0 or I.max() > 255):
         I = (I - I.min()) * (255 / (I.max() - I.min()))
         I = I.round()